5902af412d070cb5a4f6d4a4da943986dabb6af3
diogeneszilli <diogeneszilli@gmail.com>
2023-10-18 13:17:51 +0200

    handle core api request exceptions

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -14,16 +14,28 @@ export class AppService {
   }
 
   async getPlatforms(): Promise<string> {
-    const { data } = await this.httpService.axiosRef.get(
-      `${this.configService.get('CORE_API_URL')}/platforms`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'core-api-key': this.configService.get('CORE_API_KEY'),
+    try {
+      const { data } = await this.httpService.axiosRef.get(
+        `${this.configService.get('CORE_API_URL')}/platforms`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'core-api-key': this.configService.get('CORE_API_KEY'),
+          },
         },
-      },
-    );
-    data.platforms.forEach((platform) => console.log(platform));
-    return 'Successful';
+      );
+      data.platforms.forEach((platform) => console.log(platform));
+      return 'Successful';
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: error.response?.status || 503,
+          error: `Core API request: ${
+            error.response?.statusText || 'Unavailable'
+          }.`,
+        },
+        error.response?.status || 503,
+      );
+    }
   }
 }
